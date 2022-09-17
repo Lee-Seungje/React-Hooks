3a8acf904b9efb713bbbca56d54518a9b57e43a7
Seungjee Lee <moondgod@gmail.com>
2022-09-17 20:52:06 +0900

    modify

--- useState/useAxios_index.js
+++ useState/useAxios_index.js
@@ -1,17 +1,19 @@
-export const useInput = (initialValue, validator) => {
-    const [value, setValue] = useState(initialValue);
-    const onChange = (event) => {
-      const {
-        target: { value }
-      } = event;
-      let willUpdate = true;
-      if (willUpdate) {
-        setValue(value);
-      }
-      if (typeof validator === "function") {
-        willUpdate = validator(value);
-      }
-    };
-    return { value, onChange };
-  };
-  
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json"
+  });
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>refetch</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
